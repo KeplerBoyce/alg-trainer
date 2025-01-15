c34206c8d18e784161593b1e589a49aca2e68068
KeplerBoyce <keplerboy@gmail.com>
2025-01-15 11:01:30 -0500

    unplugin icons setup + modal close button

--- website/src/lib/components/Modal.svelte
+++ website/src/lib/components/Modal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import AkarIconsCross from '~icons/akar-icons/cross'
+
   export let open: boolean;
   export let close: () => void;
 
@@ -21,8 +23,19 @@
       ${open ? "opacity-100" : "opacity-0"}
       ${internalOpen ? "z-[100]" : "-z-10"}`}
   >
-    <div class={`transition duration-300 ${open ? "translate-y-0 scale-100" : "translate-y-2 scale-90"}`}>
+    <div class={`z-[110] relative transition duration-300 ${open ? "translate-y-0 scale-100" : "translate-y-2 scale-90"}`}>
       <slot />
+      <div
+        role="button"
+        tabindex={0}
+        on:click={close}
+        on:keypress={close}
+        class="absolute top-2 right-2"
+      >
+        <div class="hover:bg-slate-100 active:bg-slate-200 p-2 rounded-full">
+          <AkarIconsCross />
+        </div>
+      </div>
     </div>
   </div>
 </button>
