2c05df7520e8ef68667d12c3b66d9f7268f0cbc9
KeplerBoyce <keplerboy@gmail.com>
2025-05-28 14:15:55 -1000

    Simple navbar and trainer page created

--- website/src/lib/components/Header.svelte
+++ website/src/lib/components/Header.svelte
@@ -1,5 +1,17 @@
-<div class="fixed z-50 w-full flex justify-center bg-white/50 backdrop-blur-lg items-center h-16 min-h-16 border-b-2 drop-shadow-md">
+<script>
+  import NiceLink from "./NiceLink.svelte";
+</script>
+<div class="fixed z-50 w-full flex gap-4 justify-center bg-white/50 backdrop-blur-lg items-center h-16 min-h-16 border-b-2 drop-shadow-md">
   <p class="text-3xl font-bold">
     Algorithm Trainer
   </p>
+  <p>—</p>
+  <div class="flex gap-4 items-center">
+    <NiceLink href="/">
+      Alg List
+    </NiceLink>
+    <NiceLink href="/trainer">
+      Trainer
+    </NiceLink>
+  </div>
 </div>
---
+++ website/src/lib/components/NiceLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { page } from '$app/state';
+
+  let {
+    href,
+    children,
+  }: {
+    href: string,
+    children: Snippet,
+  } = $props();
+</script>
+
+{#if page.url.pathname === href}
+  <a href={href} class="text-black underline">
+    {@render children()}
+  </a>
+{:else}
+  <a href={href} class="transition text-blue-500 hover:text-blue-400 underline">
+    {@render children()}
+  </a>
+{/if}
