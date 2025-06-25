b401752c568c3ea801ef8bc8ca33104b3c29f1ed
KeplerBoyce <keplerboy@gmail.com>
2025-06-25 14:56:55 -0700

    Fix padding issue with button component

--- src/lib/components/NiceButton.svelte
+++ src/lib/components/NiceButton.svelte
@@ -18,14 +18,13 @@
   } = $props();
 </script>
 
-<div class={`transition rounded-lg px-4 py-2 w-min whitespace-nowrap ${disabled ? "bg-gray-200 active:bg-gray-300" : `${color} hover:${hoverColor} active:${activeColor}`}`}>
-  {#if disabled}
-    <button class="hover:cursor-default">
-      {@render children()}
-    </button>
-  {:else}
-    <button onclick={handleClick}>
-      {@render children()}
-    </button>
-  {/if}
-</div>
+<button
+  onclick={() => {
+    if (!disabled) {
+      handleClick();
+    }
+  }}
+  class={`transition rounded-lg px-4 py-2 w-min whitespace-nowrap ${disabled ? "hover:cursor-default bg-gray-200 active:bg-gray-300" : `${color} hover:${hoverColor} active:${activeColor}`}`}
+>
+  {@render children()}
+</button>
